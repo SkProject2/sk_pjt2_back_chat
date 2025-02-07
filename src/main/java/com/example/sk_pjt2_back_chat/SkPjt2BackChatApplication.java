bf9cab47347bd9690de4c4193a7d93366b8838bc
Danmark_Jajang <54851789+Danmark-Jajang@users.noreply.github.com>
2025-02-07 16:57:18 +0900

    Initial commit

---
+++ src/main/java/com/example/sk_pjt2_back_chat/SkPjt2BackChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.sk_pjt2_back_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkPjt2BackChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkPjt2BackChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sk_pjt2_back_chat/SkPjt2BackChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sk_pjt2_back_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkPjt2BackChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
